8485c685df5a5d51b0e715d565bb9911084bd367
野見山陸 <riku@nomiyamarikunoMacBook-puro.local>
2018-09-01 12:03:44 +0900

    implement basic login

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require bootstrap
 //= require activestorage
 //= require turbolinks
-//= require jquery
 //= require_tree .
